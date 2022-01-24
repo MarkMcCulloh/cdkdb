310856b601a490ab829c0eae50ea0f85b959ec9e
Mark McCulloh <Mark.McCulloh@gmail.com>
2022-01-24 01:49:34 -0500

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,15 @@
+import { cdk } from "projen";
+const project = new cdk.JsiiProject({
+  author: "Mark McCulloh",
+  authorAddress: "Mark.McCulloh@gmail.com",
+  defaultReleaseBranch: "main",
+  name: "cdkdb",
+  projenrcTs: true,
+  repositoryUrl: "https://github.com/Mark.McCulloh/cdkdb.git",
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
